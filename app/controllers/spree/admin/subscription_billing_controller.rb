0263dc8abe405248822a18d1c906006dfbb9d0d0
Agileblaze Technologies <github@agileblaze.com>
2012-02-24 15:29:39 +0530

    Update app/controllers/spree/admin/subscription_billing_controller.rb

--- app/controllers/spree/admin/subscription_billing_controller.rb
+++ app/controllers/spree/admin/subscription_billing_controller.rb
@@ -5,7 +5,7 @@ class Spree::Admin::SubscriptionBillingController < Spree::BaseController #Appli
   before_filter :load_api,:except=>['configuration','create_configuration']
 
   def index
-    #Recurly.api_key = '78256c8e627a4a829f5e3948f30012a2'
+    #Recurly.api_key = 'Specify your key '
   end
 
   def accounts
